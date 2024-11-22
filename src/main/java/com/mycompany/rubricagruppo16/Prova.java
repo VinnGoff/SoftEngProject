6dd6fa664063af5a33830fd2adf5634635d31f00
vgoff <vgoff@172.19.243.81>
2024-11-22 15:01:29 +0100

    Prova stampa vincenzo

---
+++ src/main/java/com/mycompany/rubricagruppo16/Prova.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rubricagruppo16;
+
+/**
+ *
+ * @author vgoff
+ */
+public class Prova {
+    public static void main(){
+        System.out.println("Prova vincenzo");
+    }
+}
