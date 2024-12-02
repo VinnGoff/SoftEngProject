e88b7b029e119417449afc082c06ea8958f818ae
firy <firy@fedora-firy>
2024-12-02 15:26:27 +0100

    Classe Contatto

--- src/main/java/com/mycompany/rubricagruppo16/Contatto.java
+++ src/main/java/com/mycompany/rubricagruppo16/Contatto.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.rubricagruppo16;
 
+import java.util.Arrays;
+
 /**
  *
  * @author vgoff
@@ -12,5 +14,51 @@ package com.mycompany.rubricagruppo16;
 public class Contatto {
     private String nome;
     private String cognome;
+    private int numeriTel[];
+    private String indirizziEmail[];
     
+    public Contatto(String nome, String cognome, int numeriTel[], String indirizziEmail[]){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeriTel = numeriTel;
+        this.indirizziEmail = indirizziEmail;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCognome() {
+        return cognome;
+    }
+    
+    public int[] getNumeriTel() {
+        return numeriTel;
+    }
+    
+    public String[] getIndirizziEmail() {
+        return indirizziEmail;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    
+    public void setNumeriTel(int numeriTel[]) {
+        this.numeriTel = numeriTel;
+    }
+    
+    public void setIndirizziEmail(String indirizziEmail[]) {
+        this.indirizziEmail = indirizziEmail;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return ("Nome: " + nome + "\nCognome: " + cognome + "\nNumeri di telefono: " + Arrays.toString(numeriTel) + "\nIndirizzi e-mail: " + Arrays.toString(indirizziEmail));
+    }
 }
