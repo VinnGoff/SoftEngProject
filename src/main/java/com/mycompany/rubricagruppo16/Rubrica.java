993d535d99b42447265e12a1b8fadea4b1bc594e
vgoff <vgoff@172.19.243.81>
2024-12-02 16:03:02 +0100

    modifica indentazione rubrica

--- src/main/java/com/mycompany/rubricagruppo16/Rubrica.java
+++ src/main/java/com/mycompany/rubricagruppo16/Rubrica.java
@@ -12,16 +12,27 @@ package com.mycompany.rubricagruppo16;
 import java.util.ArrayList;
 
 public class Rubrica {
-   private ArrayList<Contatto>lista;
-   public Rubrica(){
-   this.lista=new ArrayList<>();
+    private ArrayList<Contatto>lista;
+   
+    public Rubrica(){
+        this.lista=new ArrayList<>();
+    }
+    
+    public void inserisciContatto(Contatto x){
+        
+    }
+ 
+    public Contatto ricerca(String nome){
+        
+        return null;
+        
+    }
+
+    public void eliminaContatto(int index){
+       
+    }
+
+    public void stampaRubrica(){
+      
+    }
 }
-   public void inserisciContatto(Contatto x){
-}
-   public Contatto ricera(String nome){
-}
-   public void eliminaContatto(int index){
-}
-  public void stamparubrica(){
-}
-{
