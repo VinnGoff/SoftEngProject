4c7b47dc0201f752ef2de1e049afa3b0bb6590b5
vgoff <vgoff@172.19.243.81>
2024-12-02 14:43:02 +0100

    Rimozione file di prova aggiunta view, controller, contatto e rubrica come classi principali

---
+++ src/main/java/com/mycompany/rubricagruppo16/Contatto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rubricagruppo16;
+
+/**
+ *
+ * @author vgoff
+ */
+public class Contatto {
+    private String nome;
+    private String cognome;
+    
+}
