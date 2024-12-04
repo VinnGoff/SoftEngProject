c0cab14119368f81004e3ed2873880fcb151e1dd
Utente <Utente@DESKTOP-MUMLQ33>
2024-12-04 09:02:56 +0100

    FIRST COMMIT

---
+++ src/main/java/com/mycompany/rubricagruppo16/NonValidoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.rubricagruppo16;
+
+/**
+ *
+ * @author Utente
+ */
+public class NonValidoException extends RuntimeException{
+    public NonValidoException(){
+        public NonValidoException(String msg){
+            super(msg);
+    }
+}
