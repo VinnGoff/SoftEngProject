1d0ff28c527f89ad0a1659f491b8a2b9df2f226d
Utente <Utente@192.168.56.1>
2024-12-14 17:33:48 +0100

    doxygen email modificato

--- src/main/java/it/unisa/se/project/datiAndStrutture/Email.java
+++ src/main/java/it/unisa/se/project/datiAndStrutture/Email.java
@@ -1,23 +1,31 @@
 /**
  * @file Email.java
  * @brief Rappresenta un indirizzo email
- * 
+ * @package it.unisa.se.project.datiAndStrutture
  * Gestisce la validazione e formattazione degli indirizzi email
  */
 package it.unisa.se.project.datiAndStrutture;
 
+/**
+ * @brief classe Email
+ */
 public class Email {
+    /**
+     * @private Stringa indirizzo email
+     */
     private String indirizzo;
 
     /**
      * @brief Costruisce un nuovo indirizzo email
      * @param indirizzo L'indirizzo email
-     * @throws IllegalArgumentException se l'indirizzo non è valido
      */
     public Email(String indirizzo) {
         this.indirizzo = indirizzo;
     }
-
+    /**
+     * @brief toString email
+     * @return indirizzo email
+     */
     @Override
     public String toString() {
         return indirizzo;
