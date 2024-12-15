f2d6428910718d6575233cd491014425ebc4517e
Utente <Utente@192.168.56.1>
2024-12-15 18:06:07 +0100

    rubricacontrdoxygen altramodifica

--- src/main/java/it/unisa/se/project/controllers/RubricaController.java
+++ src/main/java/it/unisa/se/project/controllers/RubricaController.java
@@ -1,11 +1,14 @@
 /**
  * @file RubricaController.java
- * @brief Controller principale dell'applicazione
+ * @brief Controller principale dell'applicazione.
  * @package it.unisa.se.project.controllers
  * Gestisce l'interazione tra il modello, Rubrica, e la vista, RubricaView
  */
 package it.unisa.se.project.controllers;
-
+/**
+ * @name imports for RubricaController.java
+ * @{
+ */
 import it.unisa.se.project.datiAndStrutture.*;
 import java.io.File;
 import java.io.IOException;
@@ -32,13 +35,15 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
+/**
+ * @}
+ */
 
 /**
  *
  * @author firy
- * @brief class RubricaController con gli handle degli eventi
+ * @brief class RubricaController con handler di eventi
  * @implements Initializable per gestire controller
- * @section
  */
  
 public class RubricaController implements Initializable{
@@ -104,6 +109,8 @@ public class RubricaController implements Initializable{
     private final Rubrica rubrica = new Rubrica();    
     
     /**
+     * @name Initialize
+     * @{
      * @brief Override del metodo initialize di Initializable
      * @param url
      * @param rb 
@@ -115,47 +122,56 @@ public class RubricaController implements Initializable{
         contattoTable.setItems(contacts);
     }
     /**
-     * @subsection
-     * @brief setupTableColumns per collegare gli inserimeri field a tabella
-     */           
+     * @}
+     */  
+    /**
+     * @name setupTableColumns()
+     * @{
+     * @brief setupTableColumns per i controlli email e numeri 
+     * function private void
+     */
     private void setupTableColumns() {
         nameClm.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
         surnameClm.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCognome()));
-        
+    /**verifica numero1 della lista di numeri*/   
         num1Clm.setCellValueFactory(cellData -> {
             List<NumeroTel> numeri = cellData.getValue().getNumeriTel();
             return new SimpleStringProperty(!numeri.isEmpty() ? numeri.get(0).toString() : "");
         });
-        
+    /**verifica numero2 della lista di numeri*/         
         num2Clm.setCellValueFactory(cellData -> {
             List<NumeroTel> numeri = cellData.getValue().getNumeriTel();
             return new SimpleStringProperty(numeri.size() > 1 ? numeri.get(1).toString() : "");
         });
-        
+     /**verifica numero3 della lista di numeri*/        
         num3Clm.setCellValueFactory(cellData -> {
             List<NumeroTel> numeri = cellData.getValue().getNumeriTel();
             return new SimpleStringProperty(numeri.size() > 2 ? numeri.get(2).toString() : "");
         });
-        
+    /**verifica email1 della lista di email*/        
         mail1Clm.setCellValueFactory(cellData -> {
             List<Email> emails = cellData.getValue().getIndirizziEmail();
             return new SimpleStringProperty(!emails.isEmpty() ? emails.get(0).toString() : "");
         });
-        
+    /**verifica email2 della lista di email*/         
         mail2Clm.setCellValueFactory(cellData -> {
             List<Email> emails = cellData.getValue().getIndirizziEmail();
             return new SimpleStringProperty(emails.size() > 1 ? emails.get(1).toString() : "");
         });
-        
+    /**verifica email3 della lista di email*/       
         mail3Clm.setCellValueFactory(cellData -> {
             List<Email> emails = cellData.getValue().getIndirizziEmail();
             return new SimpleStringProperty(emails.size() > 2 ? emails.get(2).toString() : "");
         });
     }
-    
      /**
-     * @subsection
+     * @}
+     */  
+     /**
+     * @name setupTableSelection()
+     * @{
      * @brief setupTableSelection permette di selezionare singola riga di contatto
+      * function private void
      */           
     private void setupTableSelection() {
         contattoTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
@@ -165,10 +181,15 @@ public class RubricaController implements Initializable{
             }
         });
     }
-    
+     /**
+     * @}
+     */  
     /**
+     * @name handleAggiungiContatto()
+     * @{
      * @brief Gestisce l'aggiunta di un nuovo contatto
      * @return void
+     * function public void
      */
     @FXML
     public void handleAggiungiContatto() {
@@ -176,7 +197,7 @@ public class RubricaController implements Initializable{
             mostraErrore("Errore", "Nome o cognome non possono essere vuoti");
             return;
         }
-        
+    /**uso try*/    
         try {
             Contatto nuovoContatto = new Contatto(
                 nameField.getText().trim(),
@@ -192,14 +213,19 @@ public class RubricaController implements Initializable{
             rubrica.aggiungiContatto(nuovoContatto);
             aggiornaTabella();
             svuotaCampi();
-            
+    /**catch illegalArgumentException e*/        
         } catch (IllegalArgumentException e) {
             mostraErrore("Errore", "Formato non valido per numero di telefono o email");
         }
     }
-
     /**
+     * @}
+     */
+    /**
+     * @name handleModificaContatto()
+     * @{
      * @brief Gestisce la modifica di un contatto
+     * function public void
      */
     @FXML
     public void handleModificaContatto() {
@@ -209,9 +235,14 @@ public class RubricaController implements Initializable{
             handleAggiungiContatto();
         }
     }
-
     /**
+     * @}
+     */
+    /**
+     * @name handleRimuoviContatto()
+     * @{
      * @brief Gestisce l'eliminazione di un contatto
+     * function public void
      */
     @FXML
     public void handleRimuoviContatto() {
@@ -230,18 +261,28 @@ public class RubricaController implements Initializable{
             });
         }
     }
-
     /**
+     * @}
+     */
+    /**
+     * @name handleRicerca()
+     * @{
      * @brief Gestisce la ricerca dei contatti
+     * function public void
      */
     @FXML
     public void handleRicerca() {
         String query = searchField.getText();
         contacts.setAll(rubrica.cercaContatto(query));
     }
-    
     /**
+     * @}
+     */
+    /**
+     * @name handleSalvataggio
+     * @{
      * @brief Gestisce il salvataggio della rubrica
+     * function public void
      */
     @FXML
     public void handleSalvataggio() {
@@ -252,9 +293,11 @@ public class RubricaController implements Initializable{
         
         File file = fileChooser.showSaveDialog(contattoTable.getScene().getWindow());
         if (file != null) {
+            /**try*/
             try {
                 rubrica.salvaFile(file.getAbsolutePath());
                 mostraInfo("Successo", "Rubrica salvata correttamente");
+            /**catch IOException e*/
             } catch (IOException e) {
                 mostraErrore("Errore", "Errore durante il salvataggio del file: " + e.getMessage());
                 Logger.getLogger(RubricaController.class.getName())
@@ -262,9 +305,14 @@ public class RubricaController implements Initializable{
             }
         }
     }
-
     /**
+     * @}
+     */
+    /**
+     * @name handleCaricamento
+     * @{
      * @brief Gestisce il caricamento della rubrica
+     * function public void
      */
     @FXML
     public void handleCaricamento() {
@@ -275,10 +323,12 @@ public class RubricaController implements Initializable{
         
         File file = fileChooser.showOpenDialog(contattoTable.getScene().getWindow());
         if (file != null) {
+            /**try*/
             try {
                 rubrica.caricaFile(file.getAbsolutePath());
                 aggiornaTabella();
                 mostraInfo("Successo", "Rubrica caricata correttamente");
+            /**catch IOException e*/
             } catch (IOException e) {
                 mostraErrore("Errore", "Errore durante il caricamento del file: " + e.getMessage());
                 Logger.getLogger(RubricaController.class.getName())
@@ -287,26 +337,41 @@ public class RubricaController implements Initializable{
         }
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name aggiornaTabella()
+     * @{
      * @brief aggiornaTabella
      * @post contacts settato con i dati del contatto
+     * function private void
      */            
     private void aggiornaTabella() {
         contacts.setAll(rubrica.getContatti());
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name validateInputs()
+     * @{
      * @brief validateInputs
      * @return nameField o surnameField non vuoti restituisce true
+     * function private boolean
      */           
     private boolean validateInputs() {
         return !nameField.getText().trim().isEmpty() || 
                !surnameField.getText().trim().isEmpty();
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name svuotaCampi()
+     * @{
      * @brief svuotaCampi per ogni field di testo del contatto
      * @post field di contatto vuoti
+     * function private void
      */            
     private void svuotaCampi() {
         nameField.clear();
@@ -319,29 +384,39 @@ public class RubricaController implements Initializable{
         mail3Field.clear();
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name popolaCampiContatto()
+     * @{
      * @brief popolaCampiContatto per settare i field a valore o nulli
      * @param c contatto 
+     * function private void
      */            
     private void popolaCampiContatto(Contatto c) {
         nameField.setText(c.getNome());
         surnameField.setText(c.getCognome());
-        
+        /**set numeri*/
         List<NumeroTel> numeri = c.getNumeriTel();
         num1Field.setText(!numeri.isEmpty() ? numeri.get(0).toString() : "");
         num2Field.setText(numeri.size() > 1 ? numeri.get(1).toString() : "");
         num3Field.setText(numeri.size() > 2 ? numeri.get(2).toString() : "");
-        
+        /**set mail*/
         List<Email> emails = c.getIndirizziEmail();
         mail1Field.setText(!emails.isEmpty() ? emails.get(0).toString() : "");
         mail2Field.setText(emails.size() > 1 ? emails.get(1).toString() : "");
         mail3Field.setText(emails.size() > 2 ? emails.get(2).toString() : "");
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name mostraErrore()
+     * @{
      * @brief mostraErrore per caso di alert
      * @param title stringa
      * @param message stringa
+     * function private void
      */        
     private void mostraErrore(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -351,10 +426,15 @@ public class RubricaController implements Initializable{
         alert.showAndWait();
     }
     /**
-     * @subsection
+     * @}
+     */
+    /**
+     * @name mostraInfo()
+     * @{
      * @brief mostraInfo per messaggio alert
      * @param title stringa
      * @param message stringa
+     * function private void
      */    
     private void mostraInfo(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -363,4 +443,7 @@ public class RubricaController implements Initializable{
         alert.setContentText(message);
         alert.showAndWait();
     }
+    /**
+     * @}
+     */
 }
